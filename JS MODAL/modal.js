2bcaca86764bb1e0c1178af09b69691474787faf
Wesley Cruz WRK7 <wesleyc09gomes@gmail.com>
2025-06-24 17:24:21 -0300

    Modal

---
+++ JS MODAL/modal.js
@@ -0,0 +1,23 @@
+let BtnOpenModal = document.getElementById('BtnOpenModal')
+
+
+
+let BtnCloseModal = document.getElementById('BtnCloseModal')
+
+
+let Modal = document.getElementById('Modal')
+
+
+
+
+
+
+BtnOpenModal.addEventListener('click', () => (
+
+
+    Modal.classList.toggle('ModalOpen')
+
+
+))
+
+
